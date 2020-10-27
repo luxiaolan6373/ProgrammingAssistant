90ab67882c75fbcdcbe6c9d7c2446281fd2f4107
“luxiaolan6373 <3603799192@qq.com>
2020-10-27 17:10:34 +0800

    20201027

--- code_ui/code_main.py
+++ code_ui/code_main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow,QFrame
+from PyQt5.QtWidgets import QMainWindow,QFrame,QApplication
 from ui.wf_main import Ui_MainWindow
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
@@ -8,11 +8,12 @@ import time
 
 
 class init_window_main:
-    def __init__(self, window: QMainWindow, ui: Ui_MainWindow):
+    def __init__(self, window: QMainWindow, ui: Ui_MainWindow,app:QApplication):
         self.window = window
         self.ui = ui
         self.x = -70
         self.y = 0
+        self.app=app
         self.set()
 
     def set(self):
@@ -57,7 +58,7 @@ class init_window_main:
         self.ui.bt_win32.clicked.connect(self.clicked)
         self.ui.bt_flask.clicked.connect(self.clicked)
         #关闭按钮
-        self.ui.bt_gb.clicked.connect(exit)
+        self.ui.bt_gb.clicked.connect(self.app.exit)
         #具体功能按钮的信号绑定槽 因为 用lambad表达式传参数
         self.ui.bt_python_ksrm.clicked.connect(lambda: self.clicked_ksrm('python'))
         self.ui.bt_spider_ksrm.clicked.connect(lambda: self.clicked_ksrm('spider'))
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     window_main = QMainWindow()  # 主界面
     ui_main=wf_main.Ui_MainWindow()#实例化
     ui_main.setupUi(window_main)#运行里面的代码
-    init_window_main(window_main,ui_main)#初始化和对接代码功能
+    init_window_main(window_main,ui_main,app)#初始化和对接代码功能
     window_main.show()
 
 
